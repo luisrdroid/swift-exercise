cc679a0220ac0c0de6cf2bd9188595e22cca6e5d
Luis Romero <luis.romero@mobileappscompany.com>
2018-12-11 15:56:43 -0500

    Add OOP comparison

--- Exercise.playground/Contents.swift
+++ Exercise.playground/Contents.swift
@@ -6,6 +6,18 @@ func flexStrings(s1: String = "", s2: String = "") -> String {
     return s1 + s2 == "" ? "none": s1 + s2
 }
 
+//func flexStrings() {
+//    return "none"
+//}
+//
+//func flexStrings(s1:String) {
+//    return s1
+//}
+//
+//func flexStrings(s1:String, s2:String) {
+//    return s1 + s2
+//}
+
 flexStrings()
 flexStrings(s1: "One")
 flexStrings(s1: "One", s2: "Two")
